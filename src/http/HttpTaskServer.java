ff473d4b54bf1fe297fcea7d0439e45d59add1b4
Sergey Samoylov <sam0lov@yandex.ru>
2025-03-15 15:05:11 +0300

    TT#9 Создаёт скелет http сервера для обработки входящих запросов

---
+++ src/http/HttpTaskServer.java
@@ -0,0 +1,42 @@
+package http;
+
+import com.sun.net.httpserver.HttpServer;
+import http.handler.*;
+import manager.Manager;
+import manager.TaskManager;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class HttpTaskServer {
+
+    private static final int PORT = 8080;
+    private final HttpServer httpServer;
+
+    public HttpTaskServer(TaskManager taskManager) throws IOException {
+        httpServer = HttpServer.create(new InetSocketAddress(PORT), 0);
+        httpServer.createContext("/tasks", new TaskHandler(taskManager));
+        httpServer.createContext("/subtasks", new SubTaskHandler(taskManager));
+        httpServer.createContext("/epics", new EpicHandler(taskManager));
+        httpServer.createContext("/history", new HistoryHandler(taskManager));
+        httpServer.createContext("/prioritized", new PrioritizedHandler(taskManager));
+    }
+
+    public void start() {
+        System.out.println("Запускаем HTTP-сервер на " + PORT + " порту!");
+        httpServer.start();
+    }
+
+    public void stop(int delay) {
+        httpServer.stop(delay);
+        System.out.println("HTTP-сервер остановлен на " + PORT + " порту!");
+    }
+
+    public static void main(String[] args) throws IOException {
+        TaskManager taskManager = Manager.getDefault();
+        HttpTaskServer taskServer = new HttpTaskServer(taskManager);
+        taskServer.start();
+        System.out.println("HTTP-сервер запущен на " + PORT + " порту!");
+        taskServer.stop(180);
+    }
+}
---
+++ src/http/handler/TaskHandler.java
@@ -0,0 +1,19 @@
+package http.handler;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import manager.TaskManager;
+
+import java.io.IOException;
+
+public class TaskHandler extends BaseHttpHandler implements HttpHandler {
+
+    public TaskHandler(TaskManager taskManager) {
+        super(taskManager);
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        //тут будет код логики обработки и выдачи ответа
+    }
+}
