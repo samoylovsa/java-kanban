509616816aa7bc0e9b4bcb5c72a01c301c693e22
Sergey Samoylov <sam0lov@yandex.ru>
2025-03-16 19:02:27 +0300

    TT#9 Добавляет обработчик для запросов prioritized

--- src/http/handler/PrioritizedHandler.java
+++ src/http/handler/PrioritizedHandler.java
@@ -3,10 +3,14 @@ package http.handler;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import manager.TaskManager;
+import tasks.Task;
 
 import java.io.IOException;
+import java.util.List;
 
 public class PrioritizedHandler extends BaseHttpHandler implements HttpHandler {
+
+    private static final String PATH_NAME = "prioritized";
 
     public PrioritizedHandler(TaskManager taskManager) {
         super(taskManager);
@@ -14,6 +18,30 @@ public class PrioritizedHandler extends BaseHttpHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
+        System.out.print("Начинается обработка запроса: ");
 
+        String requestMethod = exchange.getRequestMethod();
+        String requestPath = exchange.getRequestURI().getPath();
+        String[] pathParts = requestPath.split("/");
+
+        System.out.println(requestMethod.toUpperCase() + " " + requestPath);
+
+        switch (requestMethod) {
+            case "GET":
+                handleGetRequest(exchange, pathParts, PATH_NAME);
+                break;
+            default:
+                sendErrorResponse(exchange, 405, "Метод не поддерживается");
+        }
+    }
+
+    private void handleGetRequest(HttpExchange exchange, String[] pathParts, String pathName) throws IOException {
+        if (isPathValid(pathParts, pathName, 2)) {
+            List<Task> prioritizedTasks = taskManager.getPrioritizedTasks();
+            String responseBody = gson.toJson(prioritizedTasks);
+            sendResponse(exchange, 200, responseBody);
+        } else {
+            sendErrorResponse(exchange, 400, "Ошибка в запросе");
+        }
     }
 }
