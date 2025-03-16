c0f2fce2b28685827328f72af51ecfeec0ad6e6d
Sergey Samoylov <sam0lov@yandex.ru>
2025-03-16 21:27:23 +0300

    TT#9 Добавляет обработчик для запросов history

--- src/http/handler/HistoryHandler.java
+++ src/http/handler/HistoryHandler.java
@@ -3,8 +3,10 @@ package http.handler;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import manager.TaskManager;
+import tasks.Task;
 
 import java.io.IOException;
+import java.util.List;
 
 public class HistoryHandler extends BaseHttpHandler implements HttpHandler {
 
@@ -16,7 +18,30 @@ public class HistoryHandler extends BaseHttpHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
+        System.out.print("Начинается обработка запроса: ");
 
+        String requestMethod = exchange.getRequestMethod();
+        String requestPath = exchange.getRequestURI().getPath();
+        String[] pathParts = requestPath.split("/");
+
+        System.out.println(requestMethod.toUpperCase() + " " + requestPath);
+
+        switch (requestMethod) {
+            case "GET":
+                handleGetRequest(exchange, pathParts, PATH_NAME);
+                break;
+            default:
+                sendErrorResponse(exchange, 405, "Метод не поддерживается");
+        }
     }
 
+    private void handleGetRequest(HttpExchange exchange, String[] pathParts, String pathName) throws IOException {
+        if (isPathValid(pathParts, pathName, 2)) {
+            List<Task> history = taskManager.getHistory();
+            String responseBody = gson.toJson(history);
+            sendResponse(exchange, 200, responseBody);
+        } else {
+            sendErrorResponse(exchange, 400, "Ошибка в запросе");
+        }
+    }
 }
